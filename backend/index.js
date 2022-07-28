1876ab130eb4a83bbda8e4356b1b17235b86701e
kemaltt <ttnckemal@gmail.com>
2022-07-28 09:46:51 +0200

    last

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 9000;
 //logging middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("uploads"));
 app.use((req, res, next) => {
   console.log("new request", req.method, req.url);
   next();
@@ -33,9 +34,6 @@ app.get("/detail/:id", (req, res) => {
     res.json(gesuchterCheckList);
     console.log(gesuchterCheckList);
   });
-  // const checklist = beitrageArray.find((b) => b.id === gesuchterCheckListId);
-  // console.log("checklist", checklist);
-  // res.json(checklist);
 });
 
 app.put("/checkedlist/:listId/:checkPointId", (req, res) => {
@@ -44,8 +42,6 @@ app.put("/checkedlist/:listId/:checkPointId", (req, res) => {
 
   readJSONFile("./models/data.json")
     .then((data) => {
-      // const newCheckList = data.find((item) => item.id === listId);
-      // return newCheckList;
       const newData = data.map((item) => {
         console.log(item.id);
         if (item.id === listId) {
@@ -60,34 +56,12 @@ app.put("/checkedlist/:listId/:checkPointId", (req, res) => {
       });
       return newData;
     })
-    // .then((newCheckList) => {
-    //   return newCheckList.checklist.find((item) => item.id === checkPointId);
-    // })
-    // .then((newCheckList) => {
-    //   newCheckList.checked = !newCheckList.checked;
-    //   return newCheckList;
-    //   console.log(newCheckList);
-    // })
+
     .then((newCheckList) => {
       writeJSONFile("./models/data.json", newCheckList).then(() => {
         res.json(newCheckList);
       });
     });
-
-  // res.json(newCheckList);
-  // const newData = data.map((item) => {
-  //   console.log(item.id);
-  //   if (item.id === id) {
-  //     item.checklist.map((checkPoint) => {
-  //       if (checkPoint.id === id) {
-  //         checkPoint.checked = !checkPoint.checked;
-  //       }
-  //       return checkPoint;
-  //     });
-  //     // item.checked = !item.checked;
-  //   }
-  //   return item;
-  // });
   console.log(newData);
 });
 
